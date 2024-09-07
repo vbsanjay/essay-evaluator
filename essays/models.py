d640e79dcea611cdb3e12b84e363601915a3e7db
vbsanjay <72456646+vbsanjay@users.noreply.github.com>
2024-09-07 17:54:52 +0530

    bug fix: body max_length removed.

--- essays/models.py
+++ essays/models.py
@@ -7,8 +7,8 @@ class Essay(models.Model):
     id = models.UUIDField(primary_key=True, unique=True, 
                           default=uuid.uuid4, editable=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
-    title = models.CharField(max_length=100)
-    body = models.TextField(max_length=500)
+    title = models.CharField(max_length=200)
+    body = models.TextField()
     submitted_at = models.DateTimeField(auto_now_add=True)
 
 
