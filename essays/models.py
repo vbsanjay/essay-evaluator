de3973e34ad27419c42bc9d5f54ba66d5cd89e25
vbsanjay <72456646+vbsanjay@users.noreply.github.com>
2024-09-07 08:47:47 +0530

    essay model created.

--- essays/models.py
+++ essays/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+import uuid
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Essay(models.Model):
+    id = models.UUIDField(primary_key=True, unique=True, 
+                          default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    title = models.CharField(max_length=100)
+    body = models.TextField(max_length=500)
+    submitted_at = models.DateTimeField(auto_now_add=True)
+
+
