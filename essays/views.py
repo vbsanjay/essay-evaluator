35fd183c802404084a9fe42ec77814d573a24fc6
vbsanjay <72456646+vbsanjay@users.noreply.github.com>
2024-09-07 17:55:13 +0530

    essay view has been created.

--- essays/views.py
+++ essays/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .forms import EssayForm
+from django.urls import reverse
 
 # Create your views here.
+def submit_essay(request):
+    form = EssayForm()
+    if request.method == "POST":
+        form = EssayForm(request.POST)
+        if form.is_valid():
+            essay = form.save(commit=False)
+            # essay.user = request.user 
+            essay.save()
+            print(essay)
+            return redirect(reverse("create_feedback", args=[essay.id]))
+
+    context = {"form":form}
+    return render(request, 'essays/submit_essay.html', context)
