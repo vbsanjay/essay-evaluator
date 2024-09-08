9009745814eb08c8d056971dccfb8ab2a3604cb7
vbsanjay <72456646+vbsanjay@users.noreply.github.com>
2024-09-08 08:11:33 +0530

    admin access updated.

--- essays/admin.py
+++ essays/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Essay
 
 # Register your models here.
+admin.site.register(Essay)
--- feedback/admin.py
+++ feedback/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Feedback, SpellingError
 
 # Register your models here.
+admin.site.register(Feedback)
+admin.site.register(SpellingError)
