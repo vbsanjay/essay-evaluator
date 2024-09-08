7912ba57807a0717e54c17c73b6c82ac72e0b1d1
vbsanjay <72456646+vbsanjay@users.noreply.github.com>
2024-09-08 08:12:04 +0530

    urls for feedback updated.

--- EssayEvaluator/urls.py
+++ EssayEvaluator/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("essays.urls")),
-    path('feedback/', include('feedback.urls')),
+    path('feedbacks/', include('feedback.urls')),
 ]
--- feedback/urls.py
+++ feedback/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("create_feedback/<uuid:essay_id>/", views.create_feedback, name="create_feedback"),
+    path('feedback/<uuid:feedback_id>/', views.feedback, name='feedback'),
+    path("create_feedback/<uuid:essay_id>/", views.create_feedback, name="create_feedback"),   
 ]
