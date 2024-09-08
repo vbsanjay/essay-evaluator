31b13a72511c3b0d28d8260270bd31266d3cde06
vbsanjay <72456646+vbsanjay@users.noreply.github.com>
2024-09-08 10:21:27 +0530

    Spelling Error models has been created.

--- feedback/models.py
+++ feedback/models.py
@@ -7,9 +7,16 @@ class Feedback(models.Model):
     id = models.UUIDField(primary_key=True, unique=True, 
                           editable=False, default = uuid.uuid4)
     essay = models.OneToOneField(Essay, on_delete=models.CASCADE, related_name="feedback")
-    spelling_error = models.TextField(null=True, blank=True)
+    spelling_errors_count = models.IntegerField(null=True, blank=True)
     content_relevance= models.BooleanField(null=True, blank=True)
     score = models.IntegerField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     
 
+class SpellingError(models.Model):
+    id = models.UUIDField(primary_key=True, 
+                          editable=False, unique=True, default=uuid.uuid4)
+    word = models.TextField(null=True, blank=True)
+    start = models.IntegerField(null=True, blank=True)
+    end = models.IntegerField(null=True, blank=True)
+    feedback = models.ForeignKey(Feedback, models.CASCADE, related_name="spelling_errors")
