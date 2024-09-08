a2ddf6d8ff20ef15756aae009d65eed0029cb08b
vbsanjay <72456646+vbsanjay@users.noreply.github.com>
2024-09-08 12:44:41 +0530

    preparing for vercel application

--- EssayEvaluator/settings.py
+++ EssayEvaluator/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-4+laa6wl*6l7ao=@_4%a$zx^lzp!e!c@au5xls+r+dt9r$d!vn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
 
 
 # Application definition
