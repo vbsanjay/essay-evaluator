ef379e4cd932b6215edb6bea9042f6821069b9a5
vbsanjay <72456646+vbsanjay@users.noreply.github.com>
2024-09-09 17:59:06 +0530

    views and urls.py file for profile updated

--- profiles/views.py
+++ profiles/views.py
@@ -2,3 +2,10 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required(login_url='account_login')
+def profile(request):
+    user = request.user
+    context = {
+        'user':user,
+    }
+    return render(request, "profiles/profile.html", context)
