0e313d38942ae9d98c9ada9c470eac17a31a76a8
vbsanjay <72456646+vbsanjay@users.noreply.github.com>
2024-09-09 17:58:44 +0530

    profile url added

--- EssayEvaluator/urls.py
+++ EssayEvaluator/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path("", include("essays.urls")),
     path('feedbacks/', include('feedback.urls')),
-    path('histories/', include('history.urls'))
+    path('histories/', include('history.urls')),
+    path("profiles/", include('profiles.urls'))
 ]
