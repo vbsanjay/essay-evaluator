0b0bfa54853240835499739dce4c03a55a5239fb
vbsanjay <72456646+vbsanjay@users.noreply.github.com>
2024-09-09 14:11:19 +0530

    path were added to detailed report.

--- history/urls.py
+++ history/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('history', views.history, name='history')
+    path('history', views.history, name='history'),
+    path('history/<uuid:essay_id>', views.detailed_report, name='detailed-report')
 ]
