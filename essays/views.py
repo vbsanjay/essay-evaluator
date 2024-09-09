7e53918dd6f4c5b521b75e0e6b84c7c0d9d6dcaa
vbsanjay <72456646+vbsanjay@users.noreply.github.com>
2024-09-09 10:32:01 +0530

    user and essay are connected

--- essays/views.py
+++ essays/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render, redirect, reverse
+from django.contrib.auth.decorators import login_required
+
 from django.http import HttpResponse
 from .forms import EssayForm
 # from django.urls import reverse
 
-# Create your views here.
+@login_required(login_url='account_login')
 def submit_essay(request):
     form = EssayForm()
     if request.method == "POST":
         form = EssayForm(request.POST)
         if form.is_valid():
             essay = form.save(commit=False)
-            # essay.user = request.user 
+            essay.user = request.user 
             essay.save()
             return redirect(reverse("create_feedback", args=[essay.id]))
 
