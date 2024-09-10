cda2ccc0bf5a80052d0c269c9daf8d60666e2758
vbsanjay <72456646+vbsanjay@users.noreply.github.com>
2024-09-10 10:31:17 +0530

    upated for serving static file without web server

--- EssayEvaluator/urls.py
+++ EssayEvaluator/urls.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -9,3 +12,5 @@ urlpatterns = [
     path('histories/', include('history.urls')),
     path("profiles/", include('profiles.urls'))
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
